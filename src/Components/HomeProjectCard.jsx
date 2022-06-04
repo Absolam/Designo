7a2c534be3406b9d0ae031564046ba268d82cf56
Aaron A <aaronabsolam@yahoo.com>
2022-06-04 00:11:49 -0400

    gh pages

--- src/Components/HomeProjectCard.jsx
+++ src/Components/HomeProjectCard.jsx
@@ -1,10 +1,15 @@
+import arrow from "../assets/shared/desktop/icon-right-arrow.svg";
+
 export const HomeProjectCards = ({ clName, header, link }) => {
   return (
     <div className="project-cards-container">
       <div className="overlay"></div>
       <div className="project-cards-text">
         <h3>{header}</h3>
-        <p>View Projects</p>
+        <div className="project-cards-link-container">
+          <p>View Projects</p>
+          <img src={arrow} alt="arrow" />
+        </div>
       </div>
       <div className={clName}></div>
     </div>
